a59997fbab2e7a58b7d580dbcc2c8d4fda1ab8a4
ganguklee <kw.lee@teamsparta.co>
2024-10-01 23:06:19 +0900

    특강 신청 정원초과 동시성 제어 - 단위 테스트 수정

--- src/domain/slot/test/slot.service.spec.ts
+++ src/domain/slot/test/slot.service.spec.ts
@@ -11,6 +11,7 @@ describe('slot service 단위 테스트', () => {
       getAllSlots: jest.fn(),
       getById: jest.fn(),
       save: jest.fn(),
+      decreaseSafely: jest.fn(),
     };
 
     slotService = new SlotService(mockSlotRepo);
@@ -23,21 +24,23 @@ describe('slot service 단위 테스트', () => {
       mockSlotRepo.getById.mockResolvedValue(null);
 
       await expect(slotService.getById(slotId)).rejects.toThrow(
-        'slot이 존재하지 않습니다.',
+        '특강이 존재하지 않습니다.',
       );
     });
   });
 
-  test('신청 가능한 slot 목록을 조회한다.', async () => {
-    const availableSlot = new Slot(1, 10, 'slot1', '김구루');
-    const unavailableSlot = new Slot(2, 0, 'slot2', '이구루');
-    const slots: Slot[] = [availableSlot, unavailableSlot];
+  describe('신청 가능한 slot 목록을 조회한다.', () => {
+    test('남은 좌석이 0보다  큰 slot만 조회된다.', async () => {
+      const availableSlot = new Slot(1, 10, 'slot1', '김구루');
+      const unavailableSlot = new Slot(2, 0, 'slot2', '이구루');
+      const slots: Slot[] = [availableSlot, unavailableSlot];
 
-    mockSlotRepo.getAllSlots.mockResolvedValue(slots);
+      mockSlotRepo.getAllSlots.mockResolvedValue(slots);
 
-    const result = await slotService.getAvailableSlots();
+      const result = await slotService.getAvailableSlots();
 
-    expect(result).toEqual([availableSlot]);
+      expect(result).toEqual([availableSlot]);
+    });
   });
 
   describe('slot의 잔여석을 차감한다.', () => {
@@ -47,18 +50,30 @@ describe('slot service 단위 테스트', () => {
       mockSlotRepo.getById.mockResolvedValue(null);
 
       await expect(slotService.decreaseRemaingSeats(slotId)).rejects.toThrow(
-        'slot이 존재하지 않습니다.',
+        '특강이 존재하지 않습니다.',
       );
     });
 
-    test('slot 의 잔여석을 차감할 수 없으면 에러 발생.', async () => {
+    test('slot 의 잔여석이 0이하면 에러 발생.', async () => {
       const slotId = 1;
       const emptySlot = new Slot(slotId, 0, 'slot1', '김구루');
 
       mockSlotRepo.getById.mockResolvedValue(emptySlot);
 
       await expect(slotService.decreaseRemaingSeats(slotId)).rejects.toThrow(
-        'slot의 잔여석을 차감할 수 없습니다.',
+        '특강에 남은 좌석이 없습니다.',
+      );
+    });
+
+    test('slot 업데이트 실패시 에러 발생.', async () => {
+      const slotId = 1;
+      const slot = new Slot(slotId, 10, 'slot1', '김구루');
+
+      mockSlotRepo.getById.mockResolvedValue(slot);
+      mockSlotRepo.decreaseSafely.mockResolvedValue(null);
+
+      await expect(slotService.decreaseRemaingSeats(slotId)).rejects.toThrow(
+        '남은 좌석이 없습니다.',
       );
     });
   });
